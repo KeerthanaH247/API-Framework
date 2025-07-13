a080dcc1e788a3b397d8c50751e773199623b09c
Keerthana H <keerthanadas247@gmail.com>
2025-07-13 19:18:45 +0530

    Report

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinations"}, tags="@DeletePlace"	)
+@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinations"}, 
+plugin = "json:target/jsonReports/cucumber-report.json"
+,tags="@DeletePlace"	)
 
 public class TestRunner {
 
